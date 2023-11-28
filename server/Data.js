0af372b6a40b057efd3019bf83b195b3f93836a5
guan <guan@localhost.localdomain>
2023-11-28 11:36:05 +0100

    Kommentarer chatgpt

--- server/Data.js
+++ server/Data.js
@@ -1,9 +1,15 @@
+// Use strict mode for better error handling
 'use strict';
-const languages = ["en", "se"];
-import {readFileSync} from "fs";
 
-// Store data in an object to keep the global namespace clean
+// Array of supported languages
+const languages = ["en", "se"];
+
+// Importing the readFileSync function from the 'fs' module
+import { readFileSync } from "fs";
+
+// Data class constructor
 function Data() {
+  // Object to store poll data
   this.polls = {};
 }
 
@@ -13,30 +19,34 @@ prototype of the Data object/class
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
 ***********************************************/
 
+// Method to retrieve questions from a JSON file
 Data.prototype.getQuestions = function () {
   const questions = readFileSync("./server/data/preparedquestions.json");
   return JSON.parse(questions);
 }
 
+// Method to retrieve UI labels based on the specified language
 Data.prototype.getUILabels = function (lang = "en") {
   const labels = readFileSync("./server/data/labels-" + lang + ".json");
   return JSON.parse(labels);
 }
 
-Data.prototype.createPoll = function(pollId, lang="en") {
+// Method to create a new poll
+Data.prototype.createPoll = function (pollId, lang = "en") {
   if (typeof this.polls[pollId] === "undefined") {
     let poll = {};
-    poll.lang = lang;  
+    poll.lang = lang;
     poll.questions = [];
     poll.answers = [];
-    poll.currentQuestion = 0;              
+    poll.currentQuestion = 0;
     this.polls[pollId] = poll;
     console.log("poll created", pollId, poll);
   }
   return this.polls[pollId];
 }
 
-Data.prototype.addQuestion = function(pollId, q) {
+// Method to add a question to an existing poll
+Data.prototype.addQuestion = function (pollId, q) {
   const poll = this.polls[pollId];
   console.log("question added to", pollId, q);
   if (typeof poll !== 'undefined') {
@@ -44,14 +54,16 @@ Data.prototype.addQuestion = function(pollId, q) {
   }
 }
 
-Data.prototype.editQuestion = function(pollId, index, newQuestion) {
+// Method to edit a question in an existing poll
+Data.prototype.editQuestion = function (pollId, index, newQuestion) {
   const poll = this.polls[pollId];
   if (typeof poll !== 'undefined') {
     poll.questions[index] = newQuestion;
   }
 }
 
-Data.prototype.getQuestion = function(pollId, qId=null) {
+// Method to get the current question in a poll
+Data.prototype.getQuestion = function (pollId, qId = null) {
   const poll = this.polls[pollId];
   console.log("question requested for ", pollId, qId);
   if (typeof poll !== 'undefined') {
@@ -60,10 +72,11 @@ Data.prototype.getQuestion = function(pollId, qId=null) {
     }
     return poll.questions[poll.currentQuestion];
   }
-  return []
+  return [];
 }
 
-Data.prototype.submitAnswer = function(pollId, answer) {
+// Method to submit an answer to the current question in a poll
+Data.prototype.submitAnswer = function (pollId, answer) {
   const poll = this.polls[pollId];
   console.log("answer submitted for ", pollId, answer);
   if (typeof poll !== 'undefined') {
@@ -73,25 +86,27 @@ Data.prototype.submitAnswer = function(pollId, answer) {
       answers[answer] = 1;
       poll.answers.push(answers);
     }
-    else if (typeof answers[answer] === 'undefined')
+    else if (typeof answers[answer] === 'undefined') {
       answers[answer] = 1;
-    else
-      answers[answer] += 1
+    }
+    else {
+      answers[answer] += 1;
+    }
     console.log("answers looks like ", answers, typeof answers);
   }
 }
 
-Data.prototype.getAnswers = function(pollId) {
+// Method to get answers for the current question in a poll
+Data.prototype.getAnswers = function (pollId) {
   const poll = this.polls[pollId];
   if (typeof poll !== 'undefined') {
     const answers = poll.answers[poll.currentQuestion];
     if (typeof poll.questions[poll.currentQuestion] !== 'undefined') {
-      return {q: poll.questions[poll.currentQuestion].q, a: answers};
+      return { q: poll.questions[poll.currentQuestion].q, a: answers };
     }
   }
-  return {}
+  return {};
 }
+
+// Export the Data class for use in other modules
 export { Data };
-
-
-
