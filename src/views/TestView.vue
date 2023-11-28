a83322aaa3cf4ecd4e3a1c6c5b590e46a03bbea8
guan <guan@localhost.localdomain>
2023-11-28 11:53:31 +0100

    En TestView

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,78 +1,49 @@
 <template>
-    <div>
-      Poll link: 
-      <input type="text" v-model="pollId">
-      <button v-on:click="createPoll">
-        Create poll
-      </button>
-      <div>
-        {{uiLabels.question}}:
-        <input type="text" v-model="question">
-        <div>
-          Answers:
-          <input v-for="(_, i) in answers" 
-                 v-model="answers[i]" 
-                 v-bind:key="'answer'+i">
-          <button v-on:click="addAnswer">
-            Add answer alternative
-          </button>
-        </div>
-      </div>
-      <button v-on:click="addQuestion">
-        Add question
-      </button>
-      <input type="number" v-model="questionNumber">
-      <button v-on:click="runQuestion">
-        Run question
-      </button>
-      {{data}}
-      <router-link v-bind:to="'/result/'+pollId">Check result</router-link>
-    </div>
-  </template>
-  
-  <script>
-  import io from 'socket.io-client';
-  const socket = io("localhost:3000");
-  
-  export default {
-    name: 'tView',
-    data: function () {
-      return {
-        lang: localStorage.getItem("lang") || "en",
-        pollId: "",
-        question: "",
-        answers: ["", ""],
-        questionNumber: 0,
-        data: {},
-        uiLabels: {}
-      }
+  <div :style="{ backgroundColor: backgroundColor }">
+    <h1 :style="{ fontSize: fontSize + 'px' }">Hello World!</h1>
+    <input type="text" v-model="inputText" @input="handleInput">
+    <button :style="{ color: buttonColor }" @click="handleClick">{{ buttonText }}</button>
+    <button @click="resetButton">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tView',
+  data() {
+    return {
+      buttonText: 'Click me',
+      buttonColor: 'black',
+      backgroundColor: 'white',
+      inputText: '',
+      fontSize: 16
+    }
+  },
+  methods: {
+    handleClick() {
+      this.buttonText = 'You clicked me!';
+      this.buttonColor = this.getRandomColor();
+      this.backgroundColor = this.getRandomColor();
     },
-    created: function () {
-      this.id = this.$route.params.id;
-      socket.emit("pageLoaded", this.lang);
-      socket.on("init", (labels) => {
-        this.uiLabels = labels
-      })
-      socket.on("dataUpdate", (data) =>
-        this.data = data
-      )
-      socket.on("pollCreated", (data) =>
-        this.data = data)
+    handleInput() {
+      this.buttonColor = this.getRandomColor();
+      this.fontSize++;
     },
-    methods: {
-      createPoll: function () {
-        socket.emit("createPoll", {pollId: this.pollId, lang: this.lang })
-      },
-      addQuestion: function () {
-        socket.emit("addQuestion", {pollId: this.pollId, q: this.question, a: this.answers } )
-      },
-      addAnswer: function () {
-        this.answers.push("");
-      },
-      runQuestion: function () {
-        socket.emit("runQuestion", {pollId: this.pollId, questionNumber: this.questionNumber})
+    resetButton() {
+      this.buttonText = 'Click me';
+      this.buttonColor = 'black';
+      this.fontSize = 16;
+      this.backgroundColor = 'white';
+      this.inputText = '';
+    },
+    getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
       }
+      return color;
     }
   }
-  </script>
-  
+}
+</script>
