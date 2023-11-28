668dbb373536a7b35fce2044b0dd5d39b6560e35
guan <guan@localhost.localdomain>
2023-11-28 11:04:55 +0100

    Chatgpt kommentarer

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import StartView from '../views/StartView.vue'
+// Importing necessary functions from Vue Router
+import { createRouter, createWebHistory } from 'vue-router';
 
+// Importing Vue component views
+import StartView from '../views/StartView.vue';
+
+// Creating a Vue Router instance
 const router = createRouter({
+  // Using web history mode and setting base URL
   history: createWebHistory(import.meta.env.BASE_URL),
+
+  // Defining routes for the application
   routes: [
     {
       path: '/',
       name: 'Start',
-      component: StartView
+      component: StartView // Default route displaying the StartView component
     },
     {
       path: '/poll/:id',
       name: 'PollView',
-      component: () => import('../views/PollView.vue')
+      component: () => import('../views/PollView.vue') // Route for displaying a poll, dynamically loading the PollView component
     },
     {
       path: '/create/',
       name: 'CreateView',
-      component: () => import('../views/CreateView.vue')
+      component: () => import('../views/CreateView.vue') // Route for creating a new poll, dynamically loading the CreateView component
     },
     {
       path: '/result/:id',
       name: 'ResultView',
-      component: () => import('../views/ResultView.vue')
+      component: () => import('../views/ResultView.vue') // Route for viewing poll results, dynamically loading the ResultView component
     }
   ]
-})
+});
 
-export default router
+// Exporting the router instance to be used in the main application
+export default router;
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,32 +1,48 @@
 <template>
   <div>
+    <!-- Poll link input field -->
     Poll link:
     <input type="text" v-model="pollId">
+
+    <!-- Create poll button -->
     <button v-on:click="createPoll">
       Create poll
     </button>
+
+    <!-- Question input and answers input fields -->
     <div>
       {{ uiLabels.question }}:
       <input type="text" v-model="question">
       <div>
         Answers:
+        <!-- Input field for each answer -->
         <input v-for="(_, i) in answers" v-model="answers[i]" v-bind:key="'answer' + i">
+        <!-- Button to add a new answer -->
         <button v-on:click="addAnswer">
           Add answer alternative
         </button>
       </div>
     </div>
+
+    <!-- Button to add a new question -->
     <button v-on:click="addQuestion">
       Add question
     </button>
+
+    <!-- Input field for question number and button to run the question -->
     <input type="number" v-model="questionNumber">
     <button v-on:click="runQuestion">
       Run question
     </button>
+
+    <!-- Display data -->
     {{ data }}
+
+    <!-- Router link to check result -->
     <router-link v-bind:to="'/result/' + pollId">Check result</router-link>
   </div>
 </template>
+
 
 <script>
 import io from 'socket.io-client';
@@ -36,6 +52,7 @@ export default {
   name: 'CreateView',
   data: function () {
     return {
+      // Initial data properties
       lang: localStorage.getItem("lang") || "en",
       pollId: "",
       question: "",
@@ -46,18 +63,28 @@ export default {
     }
   },
   created: function () {
+    // Lifecycle hook - component creation
     this.id = this.$route.params.id;
+
+    // Emit an event to the server when the page is loaded
     socket.emit("pageLoaded", this.lang);
+
+    // Listen for initialization data from the server
     socket.on("init", (labels) => {
       this.uiLabels = labels
     })
+
+    // Listen for data updates from the server
     socket.on("dataUpdate", (data) =>
       this.data = data
     )
+
+    // Listen for the event when a poll is created
     socket.on("pollCreated", (data) =>
       this.data = data)
   },
   methods: {
+    // Methods to interact with the server
     createPoll: function () {
       socket.emit("createPoll", { pollId: this.pollId, lang: this.lang })
     },
@@ -73,3 +100,4 @@ export default {
   }
 }
 </script>
+
