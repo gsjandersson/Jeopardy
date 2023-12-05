3bd0337a5991e6f02e1d6c97dd2500b01d9075cf
julplo <julia.ploman@gmail.com>
2023-12-05 14:02:27 +0100

    fix

--- src/views/PlayermodePrototyp.vue
+++ src/views/PlayermodePrototyp.vue
@@ -1,47 +1,16 @@
 <template>
-    <body>
-      <header>
-        <button id="homescreenButtonTopLeft" v-on:click="exitCreatorMode">{{ uiLabels.exit }}</button>
-        <button id="UKflagga" v-on:click="switchLanguageEnglish">{{ uiLabels.changeLanguage }}</button>
-        <button id="sverigeflagga" v-on:click="switchLanguageSwedish">{{ uiLabels.changeLanguage }}</button>
-      </header>
+    <header>
+      <h1>Create the board!</h1>
+    </header>
+    
+    <button class="exit-button" @click="exitCreatorMode">Exit Creator Mode</button>
   
-      <h1>{{ uiLabels.boardViewTitle }}</h1>
-      <p class="pollDisplay"> Poll Id: {{ pollId }}</p>
-  
-      <div>
-        Row number:
-        <input type="number" v-model="questionRow">
-        Column number:
-        <input type="number" v-model="questionColumn">
-      </div>
-      <div>
-        <button v-on:click="runQuestion"> Run question </button>
-      </div>
-  
-      <main>
-        <div class="jeopardy-board">
-        <!-- Display column titles -->
-        <div class="jeopardy-row">
-          <div v-for="(category, index) in categories" :key="index" class="jeopardy-square" @click="handleCategoryClick(index)">
-            <div v-if="!category">
-              <p>Click to add category name hej</p>
-            </div>
-            <div v-else>
-              <div>{{ category }}</div>
-            </div>
-          </div>
-        </div>
-  
-        <div>
-        <hr>
-        </div>
-  
-        <!-- Display Jeopardy board content -->
+    <main>
+      <div class="jeopardy-board">
         <div v-for="(row, indexRow) in questions" :key="indexRow" class="jeopardy-row">
           <div v-for="(col, indexCol) in row" :key="indexCol" class="jeopardy-square" @click="handleClick(indexRow, indexCol)">
             <div v-if="!col.question">
-              <p>{{ uiLabels.boardViewQuestionBox }}</p>
+              Click to Add Question
             </div>
             <div v-else>
               <div>Q: {{ col.question }}</div>
@@ -50,140 +19,82 @@
           </div>
         </div>
       </div>
-      </main>
+    </main>
   
-    </body>
+    <footer>
+      Have fun!!
+    </footer>
   </template>
   
   <script>
-  import io from 'socket.io-client';
-  const socket = io("localhost:3000");
-  
   export default {
     data() {
       return {
-        uiLabels: {},
-        lang: localStorage.getItem("lang") || "en",
         questions: Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => ({
           question: '',
           answer: ''
         }))),
-        pollId: "",
-        categories: Array.from({ length: 5 }, () => ""),
-        questionNumber: {questionRow: 0, questionColumn: 0},  
-        data: {}
       };
     },
-    created: function () {
-      // Emitting an event when the page is loaded and listening for initialization data
-      this.pollId = this.$route.params.pollId
-      socket.emit("pageLoaded", this.lang);
-      socket.on("init", (labels) => {
-        this.uiLabels = labels
-      })
-      },
     methods: {
       handleClick(row, col) {
-        let newQuestion;
-        let newAnswer;
-  
-        if (this.lang == 'en') {
-          newQuestion = prompt('Enter the question:');
-          newAnswer = prompt('Enter the correct answer:');
-        }
-        if (this.lang == 'sv') {
-          newQuestion = prompt('Skriv frågan:');
-          newAnswer = prompt('Skriv de korrekta svaret:');
-        }
-        if (newQuestion !== "" && newAnswer !== "") {
+        const newQuestion = prompt('Enter the question:');
+        const newAnswer = prompt('Enter the correct answer:');
+    
+        if (newQuestion !== null && newAnswer !== null) {
           this.questions[row][col].question = newQuestion;
           this.questions[row][col].answer = newAnswer;
-          socket.emit("addQuestion", { pollId: this.pollId, 
-            q: this.questions[row][col].question, a: this.questions[row][col].answer })
-            console.log(this)
-        }
-      },
-      handleCategoryClick(index) {
-        let categoryName;
-  
-        if (this.lang == 'en') {
-          categoryName = prompt('Enter the category name:');
-        }
-        if (this.lang == 'sv') {
-          categoryName = prompt('Skriv kategorinamnet:');
-        }
-        if (categoryName !== "") {
-          this.categories[index] = categoryName
         }
       },
       exitCreatorMode() {
-        this.$router.push('/jStartView');
+        this.$router.push('/jStartView'); 
       },
-      runQuestion: function () {
-        socket.emit("runQuestion", { pollId: this.pollId, questionNumber: this.questionNumber })
-      },
-      switchLanguageEnglish: function () {
-        if (this.lang === "sv") {
-          this.lang = "en"
-        }
-        localStorage.setItem("lang", this.lang);
-        socket.emit("switchLanguage", this.lang)
-      },
-      switchLanguageSwedish: function () {
-        if (this.lang === "en") {
-          this.lang = "sv"
-        }
-        localStorage.setItem("lang", this.lang);
-        socket.emit("switchLanguage", this.lang)
-      }
     },
   };
   </script>
   
-  <style scoped>
+  <style>
   
-  main {
-    display: flex;
-    justify-content: center;
-    padding-top: 20px;
-    margin: 0;
-  }
-  
-  .jeopardy-board {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  
-  .jeopardy-row {
-    display: flex;
-  }
-  
-  .jeopardy-square {
-    border: 1px solid #000;
-    width: 250px;
-    height: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    margin: 5px;
-  }
-  
-  footer {
-    padding-bottom: 10px;
-  }
-  
-  input {
-    margin: 2px;
-  }
-  
-  hr {
-      color: black; /* Line color */
-      background-color: black; /* Line color for older browsers */
-      height: 5px; /* Line thickness */
-      width: 1300px;
-      border: none; /* Remove the default border */
+    body {
+      background-color: #073763ff;
+      margin: 0;
+      font-family: Arial, sans-serif;
     }
   
+    main {
+      color: yellow;
+      display: flex;
+      justify-content: center;
+      margin-top: 20px;
+    }
+  
+    .exit-button {
+    color: yellow;
+    font-size: 1em;
+    background-color: #073763ff;
+    padding: 1em;
+    width: 10em;
+    text-decoration: underline;
+    }
+  
+    .jeopardy-board {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .jeopardy-row {
+      display: flex;
+    }
+  
+    .jeopardy-square {
+      border: 1px solid #000;
+      width: 250px;
+      height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      margin: 5px;
+    }
   </style>
