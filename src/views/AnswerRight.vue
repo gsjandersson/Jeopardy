445daacdca3dd49194fb49533d4991300d413e6a
idoprogram <sjnyberg@gmail.com>
2024-01-04 14:01:31 +0100

    vi testar

--- src/views/AnswerRight.vue
+++ src/views/AnswerRight.vue
@@ -2,51 +2,51 @@
   <div id="answer-box-right">
     <h1>{{ uiLabels.correct }}</h1>
     <p>{{ uiLabels.cashEarned }}: ${{ (100 * (1 + parseInt(row, 10))) }} </p>
-  </div>  
+  </div>
 </template>
 
-  <script>
-  import io from 'socket.io-client';
-  const socket = io(sessionStorage.getItem("ipAdressSocket"));
-  
-  export default {
-    name: 'AnswerRight',
-    data: function () {
-        return {
-          uiLabels: {},
-          pollId: "",
-          lang: localStorage.getItem("lang") || "en",
-          row: "",
-          participant: ""
-        }
-      },
-      created: function () {
-        this.pollId = this.$route.params.pollId
-        this.participant = this.$route.params.participantName
-        this.row = this.$route.params.row
+<script>
+import io from 'socket.io-client';
+const socket = io(sessionStorage.getItem("ipAdressSocket"));
 
-        socket.emit('joinPoll', { pollId: this.pollId, participantName: this.participant })
-    
-        socket.emit("pageLoaded", this.lang);
-        socket.on("init", (labels) => {
-          this.uiLabels = labels;
-        });
+export default {
+  name: 'AnswerRight',
+  data: function () {
+    return {
+      uiLabels: {},
+      pollId: "",
+      lang: localStorage.getItem("lang") || "en",
+      row: "",
+      participant: ""
+    }
+  },
+  created: function () {
+    this.pollId = this.$route.params.pollId
+    this.participant = this.$route.params.participantName
+    this.row = this.$route.params.row
 
-        socket.on('goToBoard', () => {
-        this.goToBoard();
-        });
+    socket.emit('joinPoll', { pollId: this.pollId, participantName: this.participant })
 
-        socket.on("goToWinnerView", () => {
-          this.$router.push(`/WinnerView/${this.pollId}`);
-        });
+    socket.emit("pageLoaded", this.lang);
+    socket.on("init", (labels) => {
+      this.uiLabels = labels;
+    });
+
+    socket.on('goToBoard', () => {
+      this.goToBoard();
+    });
+
+    socket.on("goToWinnerView", () => {
+      this.$router.push(`/WinnerView/${this.pollId}`);
+    });
+  },
+  methods: {
+    goToBoard() {
+      this.$router.push(`/jPollView/${this.pollId}/${this.participant}`);
     },
-    methods: {
-      goToBoard() {
-        this.$router.push(`/jPollView/${this.pollId}/${this.participant}`);
-        },
-      exitCreatorMode() {
-        this.$router.push('/');
-      }
+    exitCreatorMode() {
+      this.$router.push('/');
     }
   }
+}
 </script>
